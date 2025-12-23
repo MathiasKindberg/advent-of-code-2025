abde3ce98e06dc41601e96329d1723b7f5283f6f
Mathias Kindberg <mathias@kindberg.earth>
2025-12-23 17:59:40 +0000

    parse part 2 correctly...

--- days/aoc_06/src/main.rs
+++ days/aoc_06/src/main.rs
@@ -1,3 +1,5 @@
+use core::num;
+
 #[derive(Debug, Clone, Copy)]
 enum Op {
     Num(i64),
@@ -25,10 +27,11 @@ impl std::fmt::Display for Op {
     }
 }
 
-type Input = Vec<Vec<Op>>;
+type InputOne = Vec<Vec<Op>>;
 
-fn one(input: Input) {
+fn one(input: Vec<String>) {
     let now = std::time::Instant::now();
+    let input: Vec<Vec<Op>> = parse_one(&input);
     let input = aoc_lib::transpose2(input);
 
     let sum: i64 = input
@@ -47,15 +50,7 @@ fn one(input: Input) {
     println!("One: {sum} | Elapsed: {elapsed:?}");
 }
 
-fn two(_input: &Input) {
-    let now = std::time::Instant::now();
-    let sum = 0;
-
-    let elapsed = now.elapsed();
-    println!("Two: {sum} | Elapsed: {elapsed:?}");
-}
-
-fn parse(input: &[String]) -> Input {
+fn parse_one(input: &[String]) -> InputOne {
     input
         .iter()
         .map(|row| row.split_ascii_whitespace())
@@ -72,14 +67,81 @@ fn parse(input: &[String]) -> Input {
         .collect()
 }
 
+type InputTwo = Vec<Vec<String>>;
+
+fn two(input: Vec<String>) {
+    let now = std::time::Instant::now();
+    let input = parse_two(input);
+    for row in &input {
+        println!("{row:?}")
+    }
+    let input = aoc_lib::transpose2(input);
+    println!("{input:?}");
+
+    // let sum: i64 = input
+    //     .into_iter()
+    //     .map(|mut ops| {
+    //         // Rotate CCW makes last element = operand
+    //         match ops.pop().unwrap() {
+    //             Op::Addition => ops.iter().map(|op| op.num_or_panic()).sum::<i64>(),
+    //             Op::Multiplication => ops.iter().map(|op| op.num_or_panic()).product::<i64>(),
+    //             op => panic!("Last element '{op}' is not operand"),
+    //         }
+    //     })
+    //     .sum();
+
+    let elapsed = now.elapsed();
+    // println!("Two: {sum} | Elapsed: {elapsed:?}");
+}
+
+// Nasty but works.
+fn parse_two(mut input: Vec<String>) -> InputTwo {
+    let operands = input.pop().unwrap();
+    let mut number_starts: Vec<_> = operands
+        .char_indices()
+        .filter(|(_, elem)| elem != &' ')
+        .collect();
+    // Manually add the ending boundary usng the length.
+    number_starts.push((operands.len(), '#'));
+
+    let mut nums = Vec::new();
+
+    for window in number_starts[..number_starts.len()].windows(2) {
+        let mut col = Vec::new();
+
+        let num = window[0];
+        let next = window[1];
+        let size = next.0 - num.0;
+        for row in input.iter_mut() {
+            let rest_of_string = row.split_off(size);
+
+            col.push(row.clone());
+            *row = rest_of_string
+        }
+        nums.push((num.1, col));
+    }
+
+    // Remove trailing whitespace on all rows but last.
+    let num_rows = nums.len();
+    for (_, row) in nums[..num_rows - 1].iter_mut() {
+        for num in row {
+            num.pop().unwrap();
+        }
+    }
+
+    for row in nums {
+        println!("{row:?}");
+    }
+
+    todo!()
+}
+
 fn main() {
     use std::io::BufRead;
 
     let stdin = std::io::stdin();
     let input: Vec<String> = stdin.lock().lines().map_while(Result::ok).collect();
-    let input = parse(&input);
-    // println!("{input:?}");
 
     one(input.clone());
-    two(&input);
+    two(input);
 }
