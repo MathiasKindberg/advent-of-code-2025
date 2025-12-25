34094e3c27e29800288c3baedfd569cf2a6d3bbd
Mathias Kindberg <mathias@kindberg.earth>
2025-12-25 17:06:25 +0000

    day 7 part 1 done

--- days/aoc_07/src/main.rs
+++ days/aoc_07/src/main.rs
@@ -1,8 +1,59 @@
 type Input = Vec<String>;
 
-fn one(_input: &Input) {
+// Count number of splits if it has a splitter
+// above it and -1 or +1 in columns.
+fn one(input: Vec<String>) {
     let now = std::time::Instant::now();
-    let sum = 0;
+    let mut sum = 0;
+
+    let source = u64::try_from(
+        input[0]
+            .char_indices()
+            .find(|(_, elem)| elem == &'S')
+            .unwrap()
+            .0,
+    )
+    .unwrap();
+
+    let mut beam_cols: std::collections::HashSet<_> = vec![source].into_iter().collect();
+    let mut new_cols = std::collections::HashSet::new();
+
+    let mut input: Vec<_> = input
+        .into_iter()
+        .skip(1)
+        .filter_map(|row| {
+            let row = row
+                .char_indices()
+                .filter_map(|(col, elem)| match elem {
+                    '^' => Some(u64::try_from(col).unwrap()),
+                    _ => None,
+                })
+                .collect::<Vec<_>>();
+            if row.is_empty() { None } else { Some(row) }
+        })
+        .collect();
+    // We want to iterate top down
+    input.reverse();
+
+    while let Some(row) = input.pop() {
+        for splitter in row {
+            if beam_cols.contains(&splitter) {
+                // Insert the new split beamss
+                new_cols.insert(splitter - 1);
+                new_cols.insert(splitter + 1);
+
+                // Remove the terminated beam
+                beam_cols.remove(&splitter);
+                sum += 1;
+            }
+        }
+
+        // Extend the beams which did not meet a splitter.
+        new_cols.extend(beam_cols.drain());
+
+        // Swap memory around.
+        std::mem::swap(&mut beam_cols, &mut new_cols)
+    }
 
     let elapsed = now.elapsed();
     println!("One: {sum} | Elapsed: {elapsed:?}");
@@ -16,17 +67,15 @@ fn two(_input: &Input) {
     println!("Two: {sum} | Elapsed: {elapsed:?}");
 }
 
-fn parse(input: &[String]) -> Input {
-    input.iter().map(|row| row.to_owned()).collect()
-}
+// fn parse(input: &[String]) -> Input {
+//     input.iter().map(|row| row.to_owned()).collect()
+// }
 
 fn main() {
     use std::io::BufRead;
 
     let stdin = std::io::stdin();
     let input: Vec<String> = stdin.lock().lines().map_while(Result::ok).collect();
-    let input = parse(&input);
-
-    one(&input);
+    one(input.clone());
     two(&input);
 }
