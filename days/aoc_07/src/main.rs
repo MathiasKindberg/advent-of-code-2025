a44b2e724473514e8b86f9afac9100216cf02cfc
Mathias Kindberg <mathias@kindberg.earth>
2025-12-25 18:23:33 +0000

    correct part two that needs caching to not recompute already found paths.

--- days/aoc_07/src/main.rs
+++ days/aoc_07/src/main.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 type Input = Vec<String>;
 
 // Count number of splits if it has a splitter
@@ -59,12 +61,75 @@ fn one(input: Vec<String>) {
     println!("One: {sum} | Elapsed: {elapsed:?}");
 }
 
-// Typical graph finding problem. Recursively collect the number of outflowing valid paths
+// fn recurse(
+//     mut beam_cols: Vec<(u64, u64)>, // Should maybe just be (u64 u64?)
+//     splitters: std::collections::HashSet<(u64, u64)>,
+// ) -> u64 {
+//     for (beam_row, beam_col) in beam_cols {
+//         // Take a step down, if splitter then SPLIT IT!
+//         if splitters.contains(&(beam_row + 1, beam_col)) {
+//             println!("SPLIT!");
+//         }
+//     }
+//     0
+// }
+
+// Typical graph traversal problem. Recursively collect the number of outflowing valid paths
 // at each splitter. Then make it efficient by adding the numbers upward and termitnating early if
 // the path is already known.
-fn two(_input: &Input) {
+fn two(input: Vec<String>) {
     let now = std::time::Instant::now();
-    let sum = 0;
+    let mut sum = 0;
+
+    let source = u64::try_from(
+        input[0]
+            .char_indices()
+            .find(|(_, elem)| elem == &'S')
+            .unwrap()
+            .0,
+    )
+    .unwrap();
+
+    let total_rows = input.len().try_into().unwrap();
+
+    let splitters: std::collections::HashSet<(u64, u64)> = input
+        .into_iter()
+        // Remove all rows with only dots.
+        .filter(|row| !row.chars().into_iter().all(|char| char == '.'))
+        .enumerate()
+        .skip(1)
+        // Create a flat hashset of all splitters.
+        .flat_map(|(row_idx, row)| {
+            row.char_indices()
+                .filter_map(|(col_idx, elem)| match elem {
+                    '^' => Some((
+                        u64::try_from(row_idx).unwrap(),
+                        u64::try_from(col_idx).unwrap(),
+                    )),
+                    _ => None,
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect();
+
+    // This is correct but since we're double checking so many paths it takes "forever" to run.
+    let mut to_visit = vec![(0_u64, source)];
+    while let Some((beam_row, beam_col)) = to_visit.pop() {
+        let beam_row = beam_row + 1;
+        if beam_row == total_rows {
+            sum += 1;
+            continue;
+        }
+
+        if splitters.contains(&(beam_row, beam_col)) {
+            // Split!
+            to_visit.push((beam_row, beam_col - 1));
+            to_visit.push((beam_row, beam_col + 1))
+        } else {
+            // Go down!
+            to_visit.push((beam_row, beam_col));
+        }
+    }
 
     let elapsed = now.elapsed();
     println!("Two: {sum} | Elapsed: {elapsed:?}");
@@ -76,5 +141,5 @@ fn main() {
     let stdin = std::io::stdin();
     let input: Vec<String> = stdin.lock().lines().map_while(Result::ok).collect();
     one(input.clone());
-    two(&input);
+    two(input);
 }
