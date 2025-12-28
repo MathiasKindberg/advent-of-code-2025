74e059d6ad66979917be7a5fe554e3fae9a94c38
Mathias Kindberg <mathias@kindberg.earth>
2025-12-28 13:33:40 +0000

    day 10 part 1 inefficiently done.

---
+++ days/aoc_10/src/main.rs
@@ -0,0 +1,146 @@
+// Inefficient BFS, takes 21 second to run maxing out at ~17 GB memory while a DFS gets memory killed due to the exploding search space.
+fn one(input: Vec<String>) {
+    let now = std::time::Instant::now();
+    let mut sum = 0;
+    let input = parse(&input);
+
+    let mut queue = std::collections::VecDeque::new();
+    for (target_lights, buttons, _) in &input {
+        let state = vec![false; target_lights.len()];
+        let mut fewest_steps = usize::MAX;
+
+        // BFS where we terminate early if we've found a quicker path.
+        for button in buttons {
+            queue.push_back((button, state.clone(), 0));
+        }
+
+        // BFS = Pop_front
+        while let Some((button, mut state, mut steps)) = queue.pop_front() {
+            if steps >= fewest_steps {
+                continue;
+            }
+
+            // Apply change by switching buttons.
+            for change in button {
+                state[*change] = !state[*change]
+            }
+
+            steps += 1;
+            // If we've reached the target, stop searching.
+            if state == *target_lights {
+                fewest_steps = steps;
+                continue;
+            }
+
+            // Add all buttons again
+            for button in buttons {
+                queue.push_back((button, state.clone(), steps));
+            }
+        }
+        sum += fewest_steps;
+        queue.clear();
+    }
+
+    let elapsed = now.elapsed();
+    println!("One: {sum} | Elapsed: {elapsed:?}");
+}
+
+fn two(input: Vec<String>) {
+    let now = std::time::Instant::now();
+    let mut sum = 0;
+    let input = parse(&input);
+
+    let mut queue = std::collections::VecDeque::new();
+    for (target_lights, buttons, _) in &input {
+        let state = vec![false; target_lights.len()];
+        let mut fewest_steps = usize::MAX;
+
+        // BFS where we terminate early if we've found a quicker path.
+        for button in buttons {
+            queue.push_back((button, state.clone(), 0));
+        }
+
+        // BFS = Pop_front
+        while let Some((button, mut state, mut steps)) = queue.pop_front() {
+            if steps >= fewest_steps {
+                continue;
+            }
+
+            // Apply change by switching buttons.
+            for change in button {
+                state[*change] = !state[*change]
+            }
+
+            steps += 1;
+            // If we've reached the target, stop searching.
+            if state == *target_lights {
+                fewest_steps = steps;
+                continue;
+            }
+
+            // Add all buttons again
+            for button in buttons {
+                queue.push_back((button, state.clone(), steps));
+            }
+        }
+        sum += fewest_steps;
+        queue.clear();
+    }
+
+    let elapsed = now.elapsed();
+    println!("Two: {sum} | Elapsed: {elapsed:?}");
+}
+
+// Not the most efficientbut got enough.
+fn parse(input: &[String]) -> Vec<(Vec<bool>, Vec<Vec<usize>>, Vec<usize>)> {
+    input
+        .iter()
+        .map(|row| {
+            let mut iter = row.split_ascii_whitespace();
+            let lights = iter.next().unwrap();
+            let (_, lights) = lights.split_once('[').unwrap();
+            let (lights, _) = lights.split_once(']').unwrap();
+
+            let lights = lights
+                .chars()
+                .map(|light| match light {
+                    '#' => true,
+                    '.' => false,
+                    unknown => unreachable!("Got: {unknown}"),
+                })
+                .collect::<Vec<_>>();
+
+            let joltage = iter.next_back().unwrap();
+            let (_, joltage) = joltage.split_once('{').unwrap();
+            let (joltage, _) = joltage.split_once('}').unwrap();
+
+            // Skip first and last.
+            let joltage: Vec<usize> = joltage
+                .split(',')
+                .map(|joltage| joltage.parse().unwrap())
+                .collect();
+            let buttons: Vec<Vec<usize>> = iter
+                .map(|button| {
+                    let (_, button) = button.split_once('(').unwrap();
+                    let (button, _) = button.split_once(')').unwrap();
+
+                    button
+                        .split(",")
+                        .map(|light| light.parse().unwrap())
+                        .collect()
+                })
+                .collect();
+            (lights, buttons, joltage)
+        })
+        .collect()
+}
+
+fn main() {
+    use std::io::BufRead;
+
+    let stdin = std::io::stdin();
+    let input: Vec<String> = stdin.lock().lines().map_while(Result::ok).collect();
+
+    // one(input.clone());
+    two(input);
+}
